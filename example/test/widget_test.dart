9025fb2f5f9ac4dccf2e80fc36f9933b9b35fb4b
Darkmintis <darkmintis@gmail.com>
2026-08-06 14:31:01 +0545

    example: add widget smoke test

---
+++ example/test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:telor_capture_example/main.dart';
+
+void main() {
+  testWidgets('Example app renders home screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const ExampleApp());
+    expect(find.text('Welcome to MyApp'), findsOneWidget);
+  });
+}
